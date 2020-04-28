c1c82eaca61618bfb039f0eed949724b97dcf990
winderica <winderica@gmail.com>
2020-04-28 23:08:53 +0800

    Update: scripts for building fabric network

---
+++ fabric/network/network.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+export PATH=${PWD}/bin:${PWD}:$PATH
+export FABRIC_CFG_PATH=${PWD}/configtx
+export VERBOSE=false
+
+function clearContainers() {
+  docker rm -f "$(docker ps -a | awk '($2 ~ /dev-peer.*/) {print $1}')"
+}
+
+function removeUnwantedImages() {
+  docker rm -f "$(docker images | awk '($1 ~ /dev-peer.*/) {print $3}')"
+}
+
+function createOrgs() {
+  if [ -d "organizations/peerOrganizations" ]; then
+    rm -Rf organizations/peerOrganizations && rm -Rf organizations/ordererOrganizations
+  fi
+  docker-compose -f "$COMPOSE_FILE_CA" up -d 2>&1
+  . organizations/fabric-ca/registerEnroll.sh
+  sleep 10
+  createOrg1
+  createOrg2
+  createOrderer
+  ./organizations/ccp-generate.sh
+}
+
+function createConsortium() {
+  configtxgen -profile TwoOrgsOrdererGenesis -channelID system-channel -outputBlock ./system-genesis-block/genesis.block
+}
+
+function networkUp() {
+  if [ ! -d "organizations/peerOrganizations" ]; then
+    createOrgs
+    createConsortium
+  fi
+  docker-compose -f "${COMPOSE_FILE_BASE}" -f "${COMPOSE_FILE_COUCH}" up -d
+}
+
+function createChannel() {
+  if [ ! -d "organizations/peerOrganizations" ]; then
+    networkUp
+  fi
+  scripts/createChannel.sh "$CHANNEL_NAME" "$CLI_DELAY" "$MAX_RETRY" $VERBOSE
+}
+
+function deployCC() {
+  scripts/deployCC.sh "$CHANNEL_NAME" "$CC_SRC_LANGUAGE" "$VERSION" "$CLI_DELAY" "$MAX_RETRY" $VERBOSE
+  exit 0
+}
+
+function networkDown() {
+  docker-compose -f "$COMPOSE_FILE_BASE" -f "$COMPOSE_FILE_COUCH" -f "$COMPOSE_FILE_CA" down --volumes --remove-orphans
+  if [ "$MODE" != "restart" ]; then
+    clearContainers
+    removeUnwantedImages
+    rm -rf system-genesis-block/*.block organizations/peerOrganizations organizations/ordererOrganizations
+    rm -rf organizations/fabric-ca/org1/msp organizations/fabric-ca/org1/tls-cert.pem organizations/fabric-ca/org1/ca-cert.pem organizations/fabric-ca/org1/IssuerPublicKey organizations/fabric-ca/org1/IssuerRevocationPublicKey organizations/fabric-ca/org1/fabric-ca-server.db
+    rm -rf organizations/fabric-ca/org2/msp organizations/fabric-ca/org2/tls-cert.pem organizations/fabric-ca/org2/ca-cert.pem organizations/fabric-ca/org2/IssuerPublicKey organizations/fabric-ca/org2/IssuerRevocationPublicKey organizations/fabric-ca/org2/fabric-ca-server.db
+    rm -rf organizations/fabric-ca/ordererOrg/msp organizations/fabric-ca/ordererOrg/tls-cert.pem organizations/fabric-ca/ordererOrg/ca-cert.pem organizations/fabric-ca/ordererOrg/IssuerPublicKey organizations/fabric-ca/ordererOrg/IssuerRevocationPublicKey organizations/fabric-ca/ordererOrg/fabric-ca-server.db
+    rm -rf addOrg3/fabric-ca/org3/msp addOrg3/fabric-ca/org3/tls-cert.pem addOrg3/fabric-ca/org3/ca-cert.pem addOrg3/fabric-ca/org3/IssuerPublicKey addOrg3/fabric-ca/org3/IssuerRevocationPublicKey addOrg3/fabric-ca/org3/fabric-ca-server.db
+    rm -rf channel-artifacts log.txt fabcar.tar.gz fabcar
+  fi
+}
+
+MAX_RETRY=5
+CLI_DELAY=3
+CHANNEL_NAME="mychannel"
+CC_SRC_LANGUAGE=typescript
+VERSION=1
+COMPOSE_FILE_BASE=docker/docker-compose-test-net.yaml
+COMPOSE_FILE_COUCH=docker/docker-compose-couch.yaml
+COMPOSE_FILE_CA=docker/docker-compose-ca.yaml
+
+
+if [[ $# -lt 1 ]]; then
+  exit 0
+else
+  MODE=$1
+  shift
+fi
+
+if [[ $# -ge 1 ]]; then
+  key="$1"
+  if [[ "$key" == "createChannel" ]]; then
+    export MODE="createChannel"
+    shift
+  fi
+fi
+
+while [[ $# -ge 1 ]]; do
+  key="$1"
+  case $key in
+  -c)
+    CHANNEL_NAME="$2"
+    shift
+    ;;
+  -r)
+    MAX_RETRY="$2"
+    shift
+    ;;
+  -d)
+    CLI_DELAY="$2"
+    shift
+    ;;
+  -v)
+    VERSION="$2"
+    shift
+    ;;
+  esac
+  shift
+done
+
+if [ "${MODE}" == "up" ]; then
+  networkUp
+elif [ "${MODE}" == "createChannel" ]; then
+  createChannel
+elif [ "${MODE}" == "deployCC" ]; then
+  deployCC
+elif [ "${MODE}" == "down" ]; then
+  networkDown
+elif [ "${MODE}" == "restart" ]; then
+  networkDown
+  networkUp
+else
+  exit 1
+fi
---
+++ fabric/network/scripts/createChannel.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+CHANNEL_NAME="$1"
+
+export ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+export CORE_PEER_TLS_ENABLED=true
+export PEER0_ORG1_CA=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
+export PEER0_ORG2_CA=${PWD}/organizations/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+
+setGlobals() {
+  if [ "$1" -eq 1 ]; then
+    export CORE_PEER_LOCALMSPID="Org1MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:7051
+  elif [ "$1" -eq 2 ]; then
+    export CORE_PEER_LOCALMSPID="Org2MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:9051
+  fi
+}
+
+createChannelTx() {
+  configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/"${CHANNEL_NAME}".tx -channelID "$CHANNEL_NAME"
+}
+
+createAncorPeerTx() {
+  for orgmsp in Org1MSP Org2MSP; do
+    configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/${orgmsp}anchors.tx -channelID "$CHANNEL_NAME" -asOrg ${orgmsp}
+  done
+}
+
+createChannel() {
+  setGlobals 1
+  peer channel create -o localhost:7050 -c "$CHANNEL_NAME" --ordererTLSHostnameOverride orderer.example.com -f ./channel-artifacts/"${CHANNEL_NAME}".tx --outputBlock ./channel-artifacts/"${CHANNEL_NAME}".block --tls "$CORE_PEER_TLS_ENABLED" --cafile "$ORDERER_CA"
+}
+
+joinChannel() {
+  setGlobals "$1"
+  peer channel join -b ./channel-artifacts/"$CHANNEL_NAME".block
+}
+
+updateAnchorPeers() {
+  setGlobals "$1"
+  peer channel update -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com -c "$CHANNEL_NAME" -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx --tls "$CORE_PEER_TLS_ENABLED" --cafile "$ORDERER_CA"
+}
+
+FABRIC_CFG_PATH=${PWD}/configtx
+
+createChannelTx
+createAncorPeerTx
+
+FABRIC_CFG_PATH=${PWD}/config
+
+createChannel
+
+joinChannel 1
+joinChannel 2
+
+updateAnchorPeers 1
+updateAnchorPeers 2
