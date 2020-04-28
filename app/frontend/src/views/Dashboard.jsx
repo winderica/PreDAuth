8a7985593d708880920b9f06ff1960b31023eb3f
winderica <winderica@gmail.com>
2020-04-28 20:49:58 +0800

    Add: app frontend

---
+++ app/frontend/src/views/Dashboard.jsx
@@ -0,0 +1,10 @@
+import { Typography } from '@material-ui/core';
+import React from 'react';
+
+export const Dashboard = () => {
+    return (
+        <>
+            <Typography>test</Typography>
+        </>
+    );
+};
