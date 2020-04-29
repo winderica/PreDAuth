d03b2e6df3fd7f8d744bbbb36f9af3aaf5765060
winderica <winderica@gmail.com>
2020-04-29 11:40:13 +0800

    Update: basic error handling for fabric backend

--- fabric/backend/src/main.ts
+++ fabric/backend/src/main.ts
@@ -5,15 +5,19 @@ import { errorHandler } from './middlewares/errorHandler';
 import { addAdmin } from './utils/wallet';
 
 (async (hostname = '0.0.0.0', port = 4000) => {
-    await addAdmin();
-    const app = express();
-    app.use(urlencoded({ extended: true }));
-    app.use(json());
-    app.use(cors());
-    app.use(routes);
-    app.use(errorHandler);
+    try {
+        await addAdmin();
+        const app = express();
+        app.use(urlencoded({ extended: true }));
+        app.use(json());
+        app.use(cors());
+        app.use(routes);
+        app.use(errorHandler);
 
-    app.listen(port, hostname, () => {
-        console.log(`Listening on ${hostname}:${port}`);
-    });
+        app.listen(port, hostname, () => {
+            console.log(`Listening on ${hostname}:${port}`);
+        });
+    } catch (e) {
+        console.log(e);
+    }
 })();
