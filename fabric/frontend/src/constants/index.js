f2b6aef194a773302f14224ab73068dcfb7c1ff3
winderica <winderica@gmail.com>
2020-07-03 18:16:32 +0800

    Update: more API endpoints

--- fabric/frontend/src/constants/index.js
+++ fabric/frontend/src/constants/index.js
@@ -1,8 +1,12 @@
 export class API {
     static root = 'https://predauth.com:4000';
-    static register = `${this.root}/user/register`;
+    static register = (id) => `${this.root}/user/${id}`;
     static data = (id) => `${this.root}/user/${id}/data`;
+    static backup = (id) => `${this.root}/user/${id}/backup`;
+    static sendCode = (id, email) => `${this.root}/user/${id}/code/${email}`;
+    static recoverByCode = (id) => `${this.root}/user/${id}/code`;
     static getGenerators = `${this.root}/auth/generators`;
+    static getPKs = `${this.root}/auth/pks`;
     static reEncrypt = (id, redirect) => `${this.root}/auth/reEncrypt/${id}/${encodeURIComponent(redirect)}`;
 }
 
