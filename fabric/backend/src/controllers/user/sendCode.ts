71e85de7fbcb730cf4d9e39a983a95eba432bbfd
winderica <winderica@gmail.com>
2020-07-03 18:46:37 +0800

    Update: get email from url parameters

--- fabric/backend/src/controllers/user/sendCode.ts
+++ fabric/backend/src/controllers/user/sendCode.ts
@@ -1,11 +1,11 @@
 import { getContract } from '@utils/wallet';
-import { PostHandler } from '@constants/types';
+import { GetHandler } from '@constants/types';
 
-export const sendCode: PostHandler<{ id: string }> = async (req, res, next) => {
+export const sendCode: GetHandler<{ id: string; email: string }> = async (req, res, next) => {
     try {
-        const { id } = req.params;
-        const contract = await getContract('admin1'); // TODO: multiple peers
-        await contract.submitTransaction('verifyEmail', id, JSON.stringify(req.body));
+        const { id, email } = req.params;
+        const contract = await getContract('admin1');
+        await contract.submitTransaction('verifyEmail', id, JSON.stringify({ payload: { email } }));
         res.json({ ok: true });
     } catch (e) {
         next(e);
