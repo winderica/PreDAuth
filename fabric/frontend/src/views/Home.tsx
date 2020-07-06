247c07de274f8d61d83aef6238b7578935618d2e
winderica <winderica@gmail.com>
2020-07-06 21:52:04 +0800

    Update: Link -> Anchor

--- fabric/frontend/src/views/Home.tsx
+++ fabric/frontend/src/views/Home.tsx
@@ -3,8 +3,9 @@ import { Button } from '@material-ui/core';
 
 import { Dialog } from '../components/Dialog';
 import { useStores } from '../hooks/useStores';
-import { Link, RouteComponentProps } from '@reach/router';
+import { RouteComponentProps } from '@reach/router';
 import { observer } from 'mobx-react';
+import { Anchor } from '../components/Anchor';
 
 export const Home = observer<FC<RouteComponentProps>>(() => {
     const { identityStore } = useStores();
@@ -15,14 +16,16 @@ export const Home = observer<FC<RouteComponentProps>>(() => {
             setOpen={() => undefined}
             title='提示'
             content='私钥不存在，请选择：'
-            actions={<>
-                <Link to='/register'>
-                    <Button color='primary'>初次使用</Button>
-                </Link>
-                <Link to='/recover'>
-                    <Button color='primary'>找回数据</Button>
-                </Link>
-            </>}
+            actions={
+                <>
+                    <Anchor to='/register'>
+                        <Button color='primary'>初次使用</Button>
+                    </Anchor>
+                    <Anchor to='/recover'>
+                        <Button color='primary'>找回数据</Button>
+                    </Anchor>
+                </>
+            }
         />
     );
 });
