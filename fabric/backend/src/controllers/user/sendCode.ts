007c3ac5c2d9dec2d7b0bf9b19cf7aec764fd72d
winderica <winderica@gmail.com>
2020-07-07 17:22:30 +0800

    Update: send transaction to all peers

--- fabric/backend/src/controllers/user/sendCode.ts
+++ fabric/backend/src/controllers/user/sendCode.ts
@@ -4,8 +4,10 @@ import { GetHandler } from '@constants/types';
 export const sendCode: GetHandler<{ id: string; email: string }> = async (req, res, next) => {
     try {
         const { id, email } = req.params;
-        const contract = await getContract('admin1');
-        await contract.evaluateTransaction('verifyEmail', id, JSON.stringify({ payload: { email } }));
+        await Promise.all([1, 2].map(async (org) => {
+            const contract = await getContract(`admin${org}`);
+            await contract.evaluateTransaction('verifyEmail', id, JSON.stringify({ payload: { email } }));
+        }));
         res.json({ ok: true });
     } catch (e) {
         next(e);
