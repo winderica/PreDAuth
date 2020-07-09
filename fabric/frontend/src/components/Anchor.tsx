36e6d1ab5b936cae6ee60bf11ebe8abfd2c5cff9
winderica <winderica@gmail.com>
2020-07-09 13:18:38 +0800

    Refactor: separate style from anchor component
    Signed-off-by: winderica <winderica@gmail.com>

--- fabric/frontend/src/components/Anchor.tsx
+++ fabric/frontend/src/components/Anchor.tsx
@@ -1,19 +1,12 @@
 import React, { FC } from 'react';
 import { Link } from '@reach/router';
+import { useStyles } from '../styles/anchor';
 
 interface Props {
     to: string;
 }
 
-export const Anchor: FC<Props> = ({ to, children }) => (
-    <Link
-        to={to}
-        style={{
-            textDecoration: 'none',
-            outline: 'none',
-            color: 'inherit',
-        }}
-    >
-        {children}
-    </Link>
-);
+export const Anchor: FC<Props> = ({ to, children }) => {
+    const classes = useStyles();
+    return <Link to={to} className={classes.link}>{children}</Link>;
+};
