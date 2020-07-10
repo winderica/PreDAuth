2b231683c40499f2ae82177fa7e98d635f3bd79e
winderica <winderica@gmail.com>
2020-07-10 19:18:08 +0800

    Fix: stub is undefined when creating context
    Signed-off-by: winderica <winderica@gmail.com>

--- fabric/chaincode/src/ledger/private.ts
+++ fabric/chaincode/src/ledger/private.ts
@@ -2,28 +2,30 @@ import { Context } from 'fabric-contract-api';
 
 export class PrivateLedger {
     readonly #ctx: Context;
-    readonly #name: string;
 
     constructor(ctx: Context) {
         this.#ctx = ctx;
+    }
+
+    private name() {
         // https://github.com/hyperledger/fabric-chaincode-node/pull/185
-        this.#name = '_implicit_org_' + (ctx.stub as unknown as { getMspID: () => string; }).getMspID();
+        return '_implicit_org_' + (this.#ctx.stub as unknown as { getMspID: () => string; }).getMspID();
     }
 
     async set(id: string, state: string) {
         await this.#ctx.stub.putPrivateData(
-            this.#name,
+            this.name(),
             id,
             Buffer.from(state)
         );
     }
 
     async get(id: string) {
-        const data = await this.#ctx.stub.getPrivateData(this.#name, id);
+        const data = await this.#ctx.stub.getPrivateData(this.name(), id);
         return Buffer.from(data).toString('utf8');
     }
 
     async del(id: string) {
-        await this.#ctx.stub.deletePrivateData(this.#name, id);
+        await this.#ctx.stub.deletePrivateData(this.name(), id);
     }
 }
