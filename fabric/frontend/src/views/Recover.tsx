01465d5d77d45a430e6514df5783f1030d26ab92
winderica <winderica@gmail.com>
2020-07-23 13:02:04 +0800

    Update: polish styles in recover page
    Signed-off-by: winderica <winderica@gmail.com>

--- fabric/frontend/src/views/Recover.tsx
+++ fabric/frontend/src/views/Recover.tsx
@@ -1,17 +1,20 @@
-import { Button, TextField } from '@material-ui/core';
+import { Button, Card, IconButton, TextField } from '@material-ui/core';
+import { AddCircleOutline, RemoveCircleOutline } from '@material-ui/icons';
 import { Redirect, RouteComponentProps } from '@reach/router';
 import { observer } from 'mobx-react';
 import React, { ChangeEvent, FC, useState } from 'react';
 
 import { api } from '../api';
 import { useStores } from '../hooks/useStores';
+import useStyles from '../styles/recover';
 import { apiWrapper } from '../utils/apiWrapper';
 
 export const Recover = observer<FC<RouteComponentProps>>(({ navigate }) => {
     const { identityStore, userDataStore, componentStateStore } = useStores();
+    const classes = useStyles();
     const [email, setEmail] = useState('');
     const [id, setId] = useState('');
-    const [codes, setCodes] = useState(['', '']);
+    const [codes, setCodes] = useState(['']);
     const handleInputEmail = (event: ChangeEvent<HTMLInputElement>) => {
         const { value } = event.target;
         setEmail(value);
@@ -41,31 +44,47 @@ export const Recover = observer<FC<RouteComponentProps>>(({ navigate }) => {
         }
         await navigate('/register');
     }, '正在请求恢复数据', '成功获取恢复数据');
+    const handleAddField = () => {
+        setCodes((prevCodes) => [...prevCodes, '']);
+    };
+    const handleRemoveField = (index: number) => () => {
+        setCodes((prevCodes) => prevCodes.filter((_, j) => j !== index));
+    };
     return identityStore.id ? <Redirect to='/' noThrow /> : <>
-        <TextField
-            autoFocus
-            margin='dense'
-            label='id'
-            fullWidth
-            value={id}
-            onChange={handleInputId}
-        />
-        <TextField
-            margin='dense'
-            label='恢复手段'
-            fullWidth
-            value={email}
-            onChange={handleInputEmail}
-        />
-        <Button variant='contained' color='primary' onClick={handleCodeRequest}>接收验证码</Button>
-        {codes.map((i, j) => <TextField
-            margin='dense'
-            label='验证码'
-            fullWidth
-            key={j}
-            value={i}
-            onChange={handleInputCode(j)}
-        />)}
-        <Button variant='contained' color='primary' onClick={handleRecover}>恢复数据</Button>
+        <Card className={classes.card}>
+            <TextField
+                autoFocus
+                margin='dense'
+                label='id'
+                fullWidth
+                value={id}
+                onChange={handleInputId}
+            />
+            <TextField
+                margin='dense'
+                label='恢复手段'
+                fullWidth
+                value={email}
+                onChange={handleInputEmail}
+            />
+            <Button variant='contained' color='primary' onClick={handleCodeRequest} className={classes.button}>接收验证码</Button>
+        </Card>
+        <Card className={classes.card}>
+            {codes.map((i, j) => <div key={j}>
+                <TextField
+                    margin='dense'
+                    label='验证码'
+                    value={i}
+                    onChange={handleInputCode(j)}
+                />
+                <IconButton onClick={handleRemoveField(j)} disabled={codes.length <= 1}>
+                    <RemoveCircleOutline />
+                </IconButton>
+                {j === codes.length - 1 && <IconButton onClick={handleAddField}>
+                    <AddCircleOutline />
+                </IconButton>}
+            </div>)}
+            <Button variant='contained' color='primary' onClick={handleRecover} className={classes.button}>恢复数据</Button>
+        </Card>
     </>;
 });
