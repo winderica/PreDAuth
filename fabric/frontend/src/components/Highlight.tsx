d6f777250e26d6867dbfcc0b8d9754fd18131f1f
winderica <winderica@gmail.com>
2020-07-24 21:22:39 +0800

    Add: code highlighter
    Signed-off-by: winderica <winderica@gmail.com>

---
+++ fabric/frontend/src/components/Highlight.tsx
@@ -0,0 +1,17 @@
+import { highlightBlock } from 'highlight.js';
+import React, { FC, useEffect, useRef } from 'react';
+
+import { useStyles } from '../styles/highlight';
+
+export const Highlight: FC<{ language: string }> = ({ language, children }) => {
+    const codeNode = useRef<HTMLElement>(null);
+    const classes = useStyles();
+    useEffect(() => {
+        codeNode.current && highlightBlock(codeNode.current);
+    });
+    return (
+        <pre className={classes.pre}>
+            <code ref={codeNode} className={language}>{children}</code>
+        </pre>
+    );
+};
