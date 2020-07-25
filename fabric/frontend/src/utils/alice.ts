71a77cf4881c6e0ca122bfef0e57cb37f68c6562
winderica <winderica@gmail.com>
2020-07-25 21:30:51 +0800

    Update: property getters && adopt new AES
    Signed-off-by: winderica <winderica@gmail.com>

--- fabric/frontend/src/utils/alice.ts
+++ fabric/frontend/src/utils/alice.ts
@@ -7,9 +7,17 @@ export class Alice {
     readonly #g: G1;
     readonly #h: G2;
 
-    constructor(private pre: PRE, g: string, h: string) {
+    constructor(public pre: PRE, g: string, h: string) {
         this.#g = this.pre.deserialize(g, 'G1');
         this.#h = this.pre.deserialize(h, 'G2');
+    }
+
+    get g() {
+        return this.#g;
+    }
+
+    get h() {
+        return this.#h;
     }
 
     key(sk?: string): PreKeyPair {
@@ -26,32 +34,16 @@ export class Alice {
 
     async encrypt(plaintext: string, pk: string): Promise<Encrypted> {
         const aesKey = this.pre.randomGen();
-        const key = await crypto.subtle.importKey(
-            'raw',
-            new Uint8Array(aesKey.match(/.{1,2}/g)?.map(byte => parseInt(byte, 16)) ?? []),
-            {
-                name: 'AES-CBC',
-            },
-            false,
-            ['encrypt', 'decrypt']
-        );
-        const iv = crypto.getRandomValues(new Uint8Array(16));
-        const data = await crypto.subtle.encrypt(
-            {
-                name: 'AES-CBC',
-                iv
-            },
-            key,
-            new TextEncoder().encode(plaintext)
-        );
+        const aes = new AES(aesKey);
+        const data = await aes.encrypt(plaintext);
         const { ca0, ca1 } = this.pre.encrypt(aesKey, this.pre.deserialize(pk, 'G1'), this.#g, this.#h);
         return {
-            data: btoa(String.fromCharCode(...new Uint8Array(data))),
+            data,
             key: {
                 ca0: this.pre.serialize(ca0),
                 ca1: this.pre.serialize(ca1),
             },
-            iv: iv.reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), ''),
+            iv: aes.iv,
         };
     }
 
@@ -60,22 +52,7 @@ export class Alice {
             ca0: this.pre.deserialize(ca0, 'Fr'),
             ca1: this.pre.deserialize(ca1, 'G1')
         }, this.pre.deserialize(sk, 'Fr'), this.#h);
-        const key = await crypto.subtle.importKey(
-            'raw',
-            new Uint8Array(aesKey.match(/.{1,2}/g)?.map(byte => parseInt(byte, 16)) ?? []),
-            {
-                name: 'AES-CBC',
-            },
-            false,
-            ['encrypt', 'decrypt']
-        );
-        return new TextDecoder().decode(await crypto.subtle.decrypt(
-            {
-                name: 'AES-CBC',
-                iv: new Uint8Array(iv.match(/.{1,2}/g)?.map(byte => parseInt(byte, 16)) ?? []),
-            },
-            key,
-            Uint8Array.from([...atob(data)].map(i => i.charCodeAt(0)))
-        ));
+        const aes = new AES(aesKey, iv);
+        return aes.decrypt(data);
     }
 }
