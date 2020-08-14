d6e2f523652d7cfb74cc06f8bcacfd749434de5d
winderica <winderica@gmail.com>
2020-08-14 19:56:39 +0800

    Fix: `from` field should consist with `user`
    Signed-off-by: winderica <winderica@gmail.com>

--- fabric/chaincode/src/utils/mail.ts
+++ fabric/chaincode/src/utils/mail.ts
@@ -1,13 +1,6 @@
 import { createTestAccount, createTransport, getTestMessageUrl, SendMailOptions } from 'nodemailer';
 
 export const mailto = async ({ text, to, from, subject }: SendMailOptions) => {
-    const options: SendMailOptions = {
-        from,
-        to,
-        subject,
-        text,
-        html: `[PreDAuth] 您的验证码为：<b>${text as string}</b> （10分钟之内有效）`,
-    };
     if (process.env.MAIL_USER && process.env.MAIL_PASS) {
         await createTransport({
             service: 'DebugMail',
@@ -15,7 +8,13 @@ export const mailto = async ({ text, to, from, subject }: SendMailOptions) => {
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASS,
             },
-        }).sendMail(options);
+        }).sendMail({
+            from: process.env.MAIL_USER,
+            to,
+            subject,
+            text,
+            html: `[PreDAuth] 您的验证码为：<b>${text as string}</b> （10分钟之内有效）`,
+        });
     }
     const { user, pass } = await createTestAccount();
     const res = await createTransport({
@@ -26,6 +25,12 @@ export const mailto = async ({ text, to, from, subject }: SendMailOptions) => {
             user,
             pass,
         },
-    }).sendMail(options);
+    }).sendMail({
+        from,
+        to,
+        subject,
+        text,
+        html: `[PreDAuth] 您的验证码为：<b>${text as string}</b> （10分钟之内有效）`,
+    });
     console.log(getTestMessageUrl(res));
 };
